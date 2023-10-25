6821ac61b0fd11e4cdf7881e6d38ffb3cadc74bc
mbienert <martin.bienert1@gmail.com>
2023-10-25 09:17:04 +0200

    Update version

--- Controller.py
+++ Controller.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.2 alpha"
+__version__ = "0.7.0 alpha"
 
 from multiprocessing import Pool, cpu_count
 from tkinter.filedialog import askopenfilenames, askopenfilename, askdirectory
